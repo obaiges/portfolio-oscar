9a3c26e56640b4bf7283b3f346a6a26280c2a40c
obaiges <oscarbaigesr@gmail.com>
2025-04-11 13:06:48 +0200

    primer commit?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +11,34 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  mode: string = 'light'; // Valor por defecto
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // Cargar el tema inicial desde localStorage
+      const savedMode = localStorage.getItem('mode');
+      if (savedMode) {
+        this.mode = savedMode;
+      }
+      this.applyTheme();
+    }
+  }
+
+  toggleMode(): void {
+    this.mode = this.mode === 'light' ? 'dark' : 'light';
+    localStorage.setItem('mode', this.mode); // Guardamos el estado en localStorage
+    this.applyTheme();
+  }
+
+  applyTheme(): void {
+    if (this.mode === 'dark') {
+      document.body.classList.add('dark-theme');
+      document.body.classList.remove('light-theme');
+    } else {
+      document.body.classList.add('light-theme');
+      document.body.classList.remove('dark-theme');
+    }
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1100px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+  },
+  plugins: [],
+}
+
