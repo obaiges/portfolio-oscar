96cb553a0fe654a087d8fccaedccc01dce929a3b
obaiges <oscarbaigesr@gmail.com>
2026-02-04 21:17:48 +0100

    better practices

--- src/app/textos.ts
+++ src/app/textos.ts
@@ -19,19 +19,19 @@ export const textos = {
             title2: 'Responsable del Departamento de TI',
             time: 'Actualmente',
             description: 'Actualmente lidero un equipo de 6 programadores en el que estamos en continuo aprendizaje y desarrollo de nuevas tecnologías. Organizo y asumo la responsabilidad de los proyectos y la coordinación del equipo, sin olvidar mis tareas diarias como programador.',
-            pastillas: {
-                past1: 'Gestión de proyectos',
-                past2: 'Conocimientos de empresa',
-                past3: 'Automatización de procesos',
-                past4: 'Y muchas cosas más...'
-            },
+            pastillas: [
+                { past: 'Gestión de proyectos' },
+                { past: 'Conocimientos de empresa' },
+                { past: 'Automatización de procesos' },
+                { past: 'Y muchas cosas más...' }
+            ],
             description2: 'Mis labores fueron desarrollar, desplegar y mantener un sistema ERP que permite la automatización de procesos, la gestión de proyectos y la coordinación de equipos, aumentando el rendimiento un 50%. Además, hice varias landing pages para adquirir habilidades en el desarrollo de sitios web responsivos.',
-            pastillas2: {
-                past1: 'Desarrollo Front-End y Back-End',
-                past2: 'Creación de servidores',
-                past3: 'Organización y buenos hábitos',
-                past4: 'Migraciones'
-            }
+            pastillas2: [
+                { past: 'Desarrollo Front-End y Back-End' },
+                { past: 'Creación de servidores' },
+                { past: 'Organización y buenos hábitos' },
+                { past: 'Migraciones' }
+            ]
         }
     },
     en: {
@@ -54,19 +54,19 @@ export const textos = {
             title2: 'Head of IT Department',
             time: 'Now',
             description: 'I currently lead a team of 6 programmers in which we are continuously learning and developing new technologies. I organise and take responsibility for the projects and the coordination of the team, without forgetting my daily tasks as a programmer.',
-            pastillas: {
-                past1: 'Project management',
-                past2: 'Corporate knowledge',
-                past3: 'Process automation',
-                past4: 'And many more...'
-            },
-            description2: 'My tasks were developing, deploying and maintaining an ERP system that allows process automation, project management and team coordination, increasing performance by 50%. In addition, I made several landing pages to acquire skills in responsive website development.', 
-            pastillas2: {
-                past1: 'Front-End and Back-End development',
-                past2: 'Server creation',
-                past3: 'Organization and good habits',
-                past4: 'Migrations'
-            }
-        }
+            pastillas: [
+                { past: 'Project management' },
+                { past: 'Corporate knowledge' },
+                { past: 'Process automation' },
+                { past: 'And many more...' }
+            ],
+            description2: 'My tasks were developing, deploying and maintaining an ERP system that allows process automation, project management and team coordination, increasing performance by 50%. In addition, I made several landing pages to acquire skills in responsive website development.',
+            pastillas2: [
+                { past: 'Front-End and Back-End development' },
+                { past: 'Server creation' },
+                { past: 'Organization and good habits' },
+                { past: 'Migrations' }
+            ]
+        },
     }
 }
